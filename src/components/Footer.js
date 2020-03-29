031c3ee6c5584f4533d421ba4fb83080fbee3c3e
Rebeka Meszaros <akeber.m@live.com>
2020-03-29 19:54:21 +0100

    created my first components, Header & Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => (
+
+    <footer className="jumbotron mt-4 mb-0">
+            <h5>Footer stuff...</h5>
+            
+            &copy; <span id="c_year">2020</span> Created by Rebeka Meszaros
+
+    </footer>
+);
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => (
+
+    <header className="jumbotron mt-4 mb-0">
+            <h1>Table Tennis Tournament</h1>
+    </header>
+);
+export default Header;
