a8c2f90bd41b84baaf5d6950d2192ade7fa66189
Rebeka Meszaros <akeber.m@live.com>
2020-04-05 19:43:23 +0100

    Updates

---
+++ precache-manifest.c9b24534038e019b45968fcab0aedfb8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dd69e410b738828888ca3f5ea04bade",
+    "url": "/Table-Tennis-Tournament/index.html"
+  },
+  {
+    "revision": "800648c7ba3cf1c4b17b",
+    "url": "/Table-Tennis-Tournament/static/css/main.8acb9937.chunk.css"
+  },
+  {
+    "revision": "09f3c93fc6c50548e583",
+    "url": "/Table-Tennis-Tournament/static/js/2.e35394f8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Table-Tennis-Tournament/static/js/2.e35394f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "800648c7ba3cf1c4b17b",
+    "url": "/Table-Tennis-Tournament/static/js/main.974c138b.chunk.js"
+  },
+  {
+    "revision": "328a355829b59130819e",
+    "url": "/Table-Tennis-Tournament/static/js/runtime-main.7f947087.js"
+  },
+  {
+    "revision": "ab35240db23f82a8bdd7390c34ab6174",
+    "url": "/Table-Tennis-Tournament/static/media/Bellerose.ab35240d.ttf"
+  },
+  {
+    "revision": "653130f744dc943e159a4462035d328a",
+    "url": "/Table-Tennis-Tournament/static/media/ping_pong.653130f7.jpg"
+  }
+]);
